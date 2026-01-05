0ad8c34c32b32d871e75e37d7284d582027cfc9c
Abrar Shahriar <abrarshahriar9410@gmail.com>
2026-01-06 04:21:47 +0600

    retesting

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -57,9 +57,9 @@ export const sendEmail = async ({
             console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
         };
     } catch (error: any) {
-        if (envVars.NODE_ENV === "production") {
+        // if (envVars.NODE_ENV === "development") {
             console.log("email sending error", error.message);
-        };
+        // };
         throw new AppError(401, "Email error");
     }
 };
