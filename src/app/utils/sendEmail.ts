531aaf06bccadf87ab8747fd8e9141613ee212c2
Abrar Shahriar <abrarshahriar9410@gmail.com>
2026-01-06 05:12:09 +0600

    email sending debug

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -38,7 +38,7 @@ export const sendEmail = async ({
     attachments
 }: SendEmailOptions) => {
     try {
-        const templatePath = path.join(__dirname, `templates/${templateName}.ejs`);
+        const templatePath = path.join(__dirname, 'templates', `${templateName}.ejs`);
         const html = await ejs.renderFile(templatePath, templateData);
         const info = await transporter.sendMail({
             from: envVars.EMAIL_SENDER.SMTP_FROM,
@@ -57,9 +57,9 @@ export const sendEmail = async ({
             console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
         };
     } catch (error: any) {
-        // if (envVars.NODE_ENV === "development") {
+        if (envVars.NODE_ENV === "development") {
             console.log("email sending error", error.message);
-        // };
+        };
         throw new AppError(401, "Email error");
     }
 };
